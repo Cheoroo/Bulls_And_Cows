62da78551daaeea6f41b2e9ef7d68f1702ea0e39
CHEO <113337884+Cheoroo@users.noreply.github.com>
2023-05-01 20:41:38 +0900

    Revert "fix: fix input"

--- input.py
+++ input.py
@@ -1,5 +1,19 @@
-def get_user_input():
+import bulls
+import bulls_and_cows_answer
+import cow
+
+randomNum = bulls_and_cows_answer.create_answer()
+
+while True:
     guessNum = list(input("Guess your number": ))
-    return guessNum
+
+    bullsCount = bulls.bulls()
+    cowsCount = cow.cow()
+
+    print(f'bulls: {bullsCount} cows: {cowsCount}')
+
+    if bullsCount == 4:
+        print("성공했습니다")
+        break
 
 
